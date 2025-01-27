2706e27de2d0d36b5f77bcfd8d1f5c97f09638c0
1alibek <alibekgithub@gmail.com>
2025-01-27 12:58:29 +0500

    done

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css"
+import Header from "./components/Header"
+import Showcase from "./components/Showcase"
+import Category from "./components/Category"
+import Products from "./components/Products"
+import Discount from "./components/Discount"
+import Comments from "./components/Comments"
+import About from "./components/About"
+import Footer from "./components/Footer"
+
+function App() {
+    return (
+        <>
+            <Header />
+            <Showcase />
+            <Category />
+            <Products />
+            <Discount />
+            <Comments />
+            <About />
+          
+            <Footer />
+        </>
+    )
+}
+
+export default App
